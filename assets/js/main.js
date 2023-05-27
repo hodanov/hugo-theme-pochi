a99166fa5658f346290b806ca1664a94a327c431
hodanov <1031hoda@gmail.com>
2023-05-28 00:12:16 +0900

    Refactor buildTableOfContents().

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,90 +89,74 @@ function handleThemeChange() {
 }
 
 function buildTableOfContents() {
-  // Check if the page has a class named 'single-page'
   if (document.querySelector(".single-page") !== null) {
-    // Get the first h2 element under the #contents element
-    var firstH2 = document.querySelector("#contents h2");
+    const firstH2 = document.querySelector("#contents h2");
     if (firstH2 === null) {
       return;
     }
-    // Create the table of contents structure
-    var tableOfContents =
-      '<div class="index-outline"><p><span>記事内目次</span></p><ol>';
-    // Initialize variables to help determine heading level and number
-    var level = 0;
-    var currentLevel = 0;
-    var h2Number = 0;
-    var h3Number = 0;
-    var h4Number = 0;
-    var headingNumber = 0;
 
-    // Select all h2, h3, and h4 elements within the article element
-    var headings = document.querySelectorAll(
-      "article h2, article h3, article h4"
+    const headers = document.querySelectorAll(
+      "article h2, article h3, article h4, article h5, article h6"
     );
+    let tableOfContents =
+      '<div class="index-outline"><p><span>Table of Contents</span></p><ol>';
+    let level = 0;
+    let currentLevel = 0;
+    let headerCounts = [
+      0, // h2
+      0, // h3
+      0, // h4
+      0, // h5
+      0, // h6
+    ];
 
-    // Loop through each heading element
-    for (var i = 0; i < headings.length; i++) {
-      // Determine the heading level based on the tag name
-      if (headings[i].tagName === "H2") {
-        level = 0;
-      } else if (headings[i].tagName === "H3") {
-        level = 1;
-      } else if (headings[i].tagName === "H4") {
-        level = 2;
-      }
+    for (let i = 0; i < headers.length; i++) {
+      level = parseInt(headers[i].tagName.charAt(1)) - 2;
 
-      // Check if the heading level has increased or decreased since the previous iteration
-      if (currentLevel < level) {
+      while (currentLevel < level) {
         tableOfContents += "<li><ol>";
-        currentLevel = level;
-      } else if (currentLevel > level) {
+        currentLevel++;
+      }
+
+      while (currentLevel > level) {
         tableOfContents += "</ol></li>";
-        currentLevel = level;
+        currentLevel--;
       }
 
-      // Increment the heading number based on the heading level
-      switch (level) {
-        case 0:
-          h2Number++;
-          headingNumber = h2Number;
-          h3Number = 0;
-          break;
-        case 1:
-          h3Number++;
-          headingNumber = h2Number + "-" + h3Number;
-          h4Number = 0;
-          break;
-        case 2:
-          h4Number++;
-          headingNumber = h2Number + "-" + h3Number + "-" + h4Number;
-          break;
+      let headerNumber = "";
+      for (let j = 0; j <= level; j++) {
+        if (j === level) {
+          headerCounts[j]++;
+          headerCounts[j + 1] = 0;
+        }
+        if (j === 0) {
+          headerNumber += headerCounts[j];
+        } else {
+          headerNumber += "-" + headerCounts[j];
+        }
       }
 
-      // Add an ID to the heading element and append its title to the TOC structure
-      headings[i].setAttribute("id", "index-toc-" + i);
+      headers[i].setAttribute("id", "index-toc-" + i);
       tableOfContents +=
         '<li><a href="#index-toc-' +
         i +
         '">' +
-        headingNumber +
+        headerNumber +
         ". " +
-        headings[i].textContent +
+        headers[i].textContent +
         "</a></li>";
     }
 
-    // Finish building the table of contents structure
     tableOfContents += "</ol></div>";
 
     // Add the TOC to the page before the first h2 element
     firstH2.insertAdjacentHTML("beforebegin", tableOfContents);
 
     // Add the TOC to the sidebar
-    var asideEl = document.createElement("aside");
+    const asideEl = document.createElement("aside");
     asideEl.innerHTML = tableOfContents;
 
-    var sideBar = document.querySelector("#sidebar #side");
+    const sideBar = document.querySelector("#sidebar #side");
     sideBar.appendChild(asideEl);
   }
 }
